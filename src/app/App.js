a892cec7e027b67a4108be50a81c2136d374feca
4holml09 <4holml09@solent.ac.uk>
2024-10-10 16:28:14 +0100

    update to use new layout

--- src/app/App.js
+++ src/app/App.js
@@ -4,14 +4,62 @@ import Posts from '../features/posts/Posts';
 import Subreddits from '../features/subreddits/Subreddits';
 import './App.css';
 
+import {
+  Sidebar,
+  SidebarBody,
+  SidebarDivider,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarHeading,
+  SidebarItem,
+  SidebarLabel,
+  SidebarSection,
+  SidebarSpacer,
+} from '../app/components/sidebar'
+import { SidebarLayout } from '../app/components/sidebar-layout'
+import { Heading } from './components/heading';
+import {
+  MagnifyingGlassIcon,
+} from '@heroicons/react/20/solid'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Posts />
-        <Subreddits />
-      </header>
-    </div>
+    <SidebarLayout
+      sidebar={
+        <Sidebar>
+
+          <SidebarHeader>
+
+            <SidebarSection className="max-lg:hidden">
+              <SidebarItem href="/search">
+                <Heading>Little Reddit </Heading>
+              </SidebarItem>
+            </SidebarSection>
+            <SidebarDivider/>
+
+            <SidebarSection className="max-lg:hidden">
+              <SidebarItem href="/search">
+                <MagnifyingGlassIcon />
+                <SidebarLabel>Search</SidebarLabel>
+              </SidebarItem>
+            </SidebarSection>
+
+          </SidebarHeader>
+
+          <SidebarBody>
+
+            <SidebarSection>
+              <Subreddits />
+            </SidebarSection>
+            <SidebarSpacer />
+
+          </SidebarBody>
+        </Sidebar>
+      }
+    >
+      {/* The page content */}
+      <Posts />
+    </SidebarLayout>
   );
 }
 
