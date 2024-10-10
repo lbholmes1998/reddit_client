73c644e678a7320824ef7fe35d1960cfbaeb6fe4
4holml09 <4holml09@solent.ac.uk>
2024-10-10 16:28:56 +0100

    fetch sub reddits on mount, change styling

--- src/features/subreddits/Subreddits.js
+++ src/features/subreddits/Subreddits.js
@@ -1,8 +1,20 @@
 // Comp to hold list of available subreddits for the user to view. 
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { HandThumbUpIcon, ChatBubbleLeftIcon } from '@heroicons/react/20/solid'
 import { useSelector, useDispatch } from 'react-redux';
+import {
+    Sidebar,
+    SidebarBody,
+    SidebarDivider,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarHeading,
+    SidebarItem,
+    SidebarLabel,
+    SidebarSection,
+    SidebarSpacer,
+} from '../../app/components/sidebar'
 
 import {
     getSubs,
@@ -10,41 +22,25 @@ import {
     selectLoading
 } from './subredditsSlice'
 
-import styles from '../counter/Counter.module.css';
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-}
-
 export default function Subreddits() {
     // Show popular subreddits
 
     const subreddits = useSelector(selectSubreddits);
-    console.log(subreddits)
     const loading = useSelector(selectLoading)
     const dispatch = useDispatch();
 
+    useEffect(() => {
+        dispatch(getSubs())
+    }, [])
+
     return (
         <div className="flow-root max-w-xl">
-            <button
-                className={styles.asyncButton}
-                onClick={() => dispatch(getSubs())}
-            >
-                Fetch most popular subreddits
-            </button>
-            {Object.keys(subreddits).length > 0 && (
-                <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
-                    {Object.values(subreddits).map((subreddit) => (
-                        <li
-                            key={subreddit.id}
-                            className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
-                        >
-                            <img alt="" src={subreddit.icon} className="mx-auto h-12 w-12 flex-shrink-0" />
-                            <p>{subreddit.url}</p>
-                        </li>
-                    ))}
-                </ul>
-            )}
-
+            {Object.values(subreddits).map((subreddit) => (
+                <SidebarItem key={subreddit.id}>
+                    <img alt="Subreddit icon" src={subreddit.icon} className="mx-auto w-6 h-6"/>
+                    <SidebarLabel>{subreddit.url}</SidebarLabel>
+                </SidebarItem>
+            ))}
         </div>
     )
 }
